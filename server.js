6295fadad54cf77637b3b2b0a6c64bb097ce6b35
Tehzeeb2487 <tehzeebjahan820@gmail.com>
2026-06-01 21:20:43 +0530

    Adding ENV

--- server.js
+++ server.js
@@ -7,48 +7,11 @@ app.use(cors());
 app.use(express.json());
 
 app.post('/send-email', async (req, res) => {
-    try {
-        console.log("Request body:", req.body);
-    
-        const transporter = nodemailer.createTransport({
-            host: "smtp.gmail.com",
-            port: 465,
-            secure: true,
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS,
-            },
-            connectionTimeout: 30000,
-            greetingTimeout: 30000,
-            socketTimeout: 30000,
-        });
+    console.log("Request body:", req.body);
 
-        console.log("EMAIL_USER:", process.env.EMAIL_USER);
-        console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
-
-        console.log("Verifying transporter...");
-
-        await transporter.verify();
-
-        console.log("Transporter verified");
-
-        const info = await transporter.sendMail({
-            from: 'javedshikh312@gmail.com',
-            to: 'javedshikh312@gmail.com',
-            subject: `Message from ${req.body.name}`,
-            text: `
-                Name: ${req.body.name}
-                Email: ${req.body.email}
-                ${req.body.message}
-            `,
-        });
-        console.log("Mail sent:", info.messageId);
-
-        res.status(200).send({ message: "Email sent successfully" });
-    } catch (error) {
-        console.error("MAIL ERROR:", error);
-        res.status(500).send({ message: error.message });
-    }
+    res.status(200).json({
+        message: "Backend working"
+    });
 });
 
 const PORT = process.env.PORT || 4000;
